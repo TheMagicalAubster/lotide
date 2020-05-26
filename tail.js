4fde63212a3bfcc139fc4d7c762959d213479b45
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-05-26 03:09:24 +0000

    tail of array

---
+++ tail.js
@@ -0,0 +1,22 @@
+const tail = function(array) {
+  let words = [];
+  for (let i = 1; i <= array.length; i++) {
+    words.push(array[i]);
+  }
+  return words;
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+    console.log(` ${actual} !== ${expected}`);
+  } else {
+    console.log(` \uD83D\uDE00 ${actual}  === ${expected}`);
+  }
+  
+};
+  
+// TEST CODE
+assertEqual(tail([5,6,7]), [5,6,7]);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Hello", "Lighthouse", "Labs"]);
+assertEqual(tail([1]), []);
