f38c51696730fb940714e7c0ef541868e14de261
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-05-27 20:35:50 +0000

    counting names .exit q quit lotout logout .logout

---
+++ countOnly.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+
+    if (actual !== expected) {
+      console.log(` ${actual} !== ${expected}`);
+    } else {
+      console.log(` \uD83D\uDE00 ${actual}  === ${expected}`);
+    }
+  
+  };
+  
+
+  //countOnly function below this line:
+
+  const countOnly = function(allItems, itemsToCount) {
+//the idea is to count the number of times a particular string shows up
+//and then return at number 
+const results = {}
+
+for (const item of allItems) {
+    console.log(item);
+if (itemsToCount[item]){
+    if (results[item]) {
+        results[item] += 1;
+    
+    } else {
+        results[item] = 1;
+    }
+}
+}
+return results;
+
+
+  }
+// TEST CODE BELOW
+const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+  
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
